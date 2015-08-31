3014f9c6892c3b5177fdfc049874d8a1a3615129
Hugo Miranda Mac <hugo@aioninnovations.com>
2015-08-31 12:06:58 -0700

    remove unused extra tests

--- number_to_human_test.go
+++ number_to_human_test.go
@@ -28,13 +28,5 @@ func TestNumberToHuman(t *testing.T) {
 	a.Equal("1234", NumberToHuman(1234, "precision:4"))
 	a.Equal("0.01234", NumberToHuman(1234, "precision:5"))
 	a.Equal("0,01234", NumberToHuman(1234, "precision:5", "separator:,"))
-	// a.Equal("12.3 Thousand", NumberToHuman(12345))
-	// a.Equal("123.4 Thousand", NumberToHuman(123456))
-
-	// a.Equal("1.234 Million", NumberToHuman(1234567))
-	// a.Equal("12.3 Million", NumberToHuman(12345678))
-	// a.Equal("123.4 Million", NumberToHuman(123456789))
-
-	// a.Equal("123 Million", NumberToHuman(123000000))
 
 }
